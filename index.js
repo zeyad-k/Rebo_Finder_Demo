04e242b1a21a54145e6120f40e81675d8a676928
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-02-28 02:28:00 +0200

    add submitButton Functionality

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log(2222);
+
+const submitButton = document.querySelector(".button");
+
+const performSearch = () => {
+	console.log(2222);
+};
+
+submitButton.addEventListener("click", (event) => {
+	event.preventDefault();
+	performSearch();
+});
