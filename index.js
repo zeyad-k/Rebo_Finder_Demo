532bb12852c1ec934453d119906f7a0c07ecc74f
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-02-28 03:39:10 +0200

    send data and perform search

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 // console.log(2222);
 
-const submitButton = document.querySelector(".button");
 
-const performSearch = () => {
-	console.log(2222);
+
+const API_URL_STATIC_PART = 'https://api.github.com/search/users?q='
+
+const setSearchResult = (response) => {
+	console.log('Show Result');
 };
+
+const performSearch = (githubUser, isUserSelected) => {
+
+	const githubUserType = isUserSelected ? '+type:user' : '+type:org';
+	// console.log(`${githubUser}  +  ${githubUserType}`);
+
+	fetch(`${API_URL_STATIC_PART} ${githubUser}${githubUserType}`)
+		.then((result) => result.json)
+		.then((response) => setSearchResult(response));
+
+};
+
+const submitButton = document.querySelector(".button");
+const githubUserElement = document.querySelector(".input");
+const githubUserTypeElement = document.querySelector('input[value="users"]');
 
 submitButton.addEventListener("click", (event) => {
 	event.preventDefault();
-	performSearch();
+	performSearch(githubUserElement.value, githubUserTypeElement.checked);
 });
+
+
